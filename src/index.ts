bf1659acc787fa51cf69e879d0daf50db5c3b04b
Robert Horvick <robert.horvick@gmail.com>
2022-09-20 20:45:08 -0400

    basics working - time to make the settings window look 20% decent

--- src/index.ts
+++ src/index.ts
@@ -13,15 +13,74 @@ craft.env.setListener((env) => {
 })
 
 window.addEventListener("load", () => {
-  const button = document.getElementById('btn-execute');
+  const generateButton: HTMLButtonElement = document.getElementById('btn-generate') as HTMLButtonElement;
+  generateButton.onclick = () => { onGenerate(); }
 
-  button?.addEventListener("click", () => {
-    const block = craft.blockFactory.textBlock({
-      content: "Hello world!"
-    });
-
-    craft.dataApi.addBlocks([block]);
-  })
   const logoImg = document.getElementById('craftx-logo') as HTMLImageElement
   logoImg.src = craftXIconSrc;
-})
+});
+
+function onGenerate() {
+  console.log("onGenerate called");
+
+  const token = (<HTMLInputElement>document.getElementById('openai-token')).value;
+  const prompt = (<HTMLInputElement>document.getElementById('openai-prompt')).value;
+
+  const query = buildOAIQuery(prompt, 'text-babbage-001');
+
+  const promptBlock = craft.blockFactory.textBlock({
+    content: query.prompt
+  });
+
+  craft.dataApi.addBlocks([promptBlock]);
+
+  fetch('https://api.openai.com/v1/completions', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token
+      },
+      body: JSON.stringify(query)
+    }
+  )
+  .then((response) => response.json())
+  .then((data) => {
+    console.log('Success:', data.choices[0].text);
+
+    const contentBlock = craft.blockFactory.textBlock({
+      content: data.choices[0].text
+    });
+
+    craft.dataApi.addBlocks([contentBlock]);
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
+}
+
+type OpenAIModel = 'text-davinci-002' | 'text-curie-001' | 'text-babbage-001' | 'text-ada-001';
+
+type OpenAIQuery = {
+  model: OpenAIModel;
+  prompt: string;
+  temperature: number;
+  max_tokens: number;
+  top_p: number;
+  frequency_penalty: number;
+  presence_penalty: number;
+}
+
+function buildOAIQuery(promptStr: string, modelStr: OpenAIModel) {
+  const model: OpenAIQuery =
+  {
+    model: modelStr,
+    prompt: promptStr,
+    temperature: 0.7,
+    max_tokens: 256,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  };
+
+  return model;
+}
